40af1e61e2a340d6bc625183f46de8cc4b8dbe77
robby oktahidayat <robbyoktahidayat@gmail.com>
2022-06-10 18:21:02 +0700

    done:challenge 07- tambah try-catch

--- promise.js
+++ promise.js
@@ -1,18 +1,17 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = async (status) =>{
+const promiseOutput = async (status) => {
   try {
     let dataIXX = await promiseTheaterIXX();
-    let datavgc= await promiseTheaterVGC();
-  
-    let dataAll = dataIXX.concat(  datavgc);
-    
-    return dataAll.filter((e) => e.hasil == status ).length;
+    let datavgc = await promiseTheaterVGC();
+
+    let dataAll = dataIXX.concat(datavgc);
+
+    return dataAll.filter((e) => e.hasil == status).length;
   } catch (error) {
     console.log(error);
   }
- 
 };
 
 module.exports = {
