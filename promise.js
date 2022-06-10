ef793e735c2805fd3b96965f37e9889db4832e07
robby oktahidayat <robbyoktahidayat@gmail.com>
2022-06-10 18:17:57 +0700

    done:challenge 07

--- promise.js
+++ promise.js
@@ -1,7 +1,14 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (status) =>{
+  let dataIXX = await promiseTheaterIXX();
+  let datavgc= await promiseTheaterVGC();
+
+  let dataAll = dataIXX.concat(  datavgc);
+  
+  return dataAll.filter((e) => e.hasil == status ).length;
+};
 
 module.exports = {
   promiseOutput,
